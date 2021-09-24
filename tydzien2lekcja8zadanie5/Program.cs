fee7497e7f5a8f010a26e2078c936391e2acd13b
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-24 11:38:32 +0200

    Add project files.

---
+++ tydzien2lekcja8zadanie5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tydzien2lekcja8zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+                do
+            {
+                Console.WriteLine($"{a*a*a}");
+                a++;
+                
+            } while (a > 0 && a < 21);
+        }
+    }
+}
